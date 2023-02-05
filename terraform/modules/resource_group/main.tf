050b006ef49dbfb6309fb09cd8a01d509cba2ab1
Christos Galanopoulos <xrholy11@gmail.com>
2023-02-05 12:32:16 +0200

    Update content

--- terraform/modules/resource_group/main.tf
+++ terraform/modules/resource_group/main.tf
@@ -2,4 +2,12 @@ resource "azurerm_resource_group" "rg" {
   name     = var.name
   location = var.location
   tags     = var.tags
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to tags, e.g. because a management agent
+      # updates these based on some ruleset managed elsewhere.
+      tags,
+    ]
+  }
 }
