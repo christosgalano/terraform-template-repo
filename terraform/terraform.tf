b500abc0f90fb8b7ba683aaacb2e4b5bcaadf428
Christos Galanopoulos <xrholy11@gmail.com>
2023-02-06 23:41:57 +0200

    Update content

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -15,3 +15,13 @@ terraform {
     }
   }
 }
+
+provider "azurerm" {
+  use_oidc = true
+
+  client_id       = var.client_id
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+
+  features {}
+}
