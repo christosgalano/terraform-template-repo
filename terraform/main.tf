2363f453c4f7af5cd74d2c98e0df492b9b94c883
Christos Galanopoulos <xrholy11@gmail.com>
2023-02-06 22:49:31 +0200

    Update content

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,15 +7,20 @@ locals {
 }
 
 locals {
-  # tflint-ignore: terraform_unused_declarations
   suffix_clean = replace(local.suffix, "-", "")
   rg_tags      = merge(var.rg_tags, local.default_tags)
 }
 
 module "resource_group" {
-  source = "./modules/resource_group"
-
+  source   = "./modules/resource_group"
   name     = "rg-${local.suffix}"
   location = var.location
   tags     = local.rg_tags
+}
+
+module "identity" {
+  source              = "./modules/identity"
+  name                = "id-${local.suffix}"
+  location            = var.location
+  resource_group_name = module.resource_group.name
 }
