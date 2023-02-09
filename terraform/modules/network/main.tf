d2e0f643b144ba91d26a2f408e4b344cf586d294
Christos Galanopoulos <xrholy11@gmail.com>
2023-02-09 17:07:33 +0200

    Update content

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -3,12 +3,13 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.location
   resource_group_name = var.resource_group_name
   address_space       = var.address_space
+  tags                = var.tags
+}
 
-  subnet {
-    count          = length(var.subnets)
-    name           = var.subnets[count.index].name
-    address_prefix = [var.subnets[count.index].address_prefix]
-  }
-
-  tags = var.tags
+resource "azurerm_subnet" "subnet" {
+  count                = length(var.subnets)
+  name                 = var.subnets[count.index].name
+  address_prefixes     = [var.subnets[count.index].address_prefix]
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
 }
