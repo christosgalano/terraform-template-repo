ee0fbd8eef1d71d1db0a5b5de40c467afa4ae728
Christos Galanopoulos <xrholy11@gmail.com>
2023-02-09 17:02:26 +0200

    Update content

--- terraform/environments/development/main.tf
+++ terraform/environments/development/main.tf
@@ -31,3 +31,21 @@ module "role_assignment" {
   role_definition_name = "Contributor"
   principal_id         = module.identity.principal_id
 }
+
+module "network" {
+  source              = "../../modules/network"
+  name                = "vnet-${local.suffix}"
+  location            = var.location
+  resource_group_name = module.resource_group.name
+  address_space       = ["10.0.0.0/24"]
+  subnets = [
+    {
+      name           = "snet-${local.suffix}-01",
+      address_prefix = "10.0.0.0/25"
+    },
+    {
+      name           = "snet-${local.suffix}-02",
+      address_prefix = "10.0.0.128/25"
+    }
+  ]
+}
