8bb11904eca6cb6d712ca40a69813483e0ca4654
Christos Galanopoulos <xrholy11@gmail.com>
2023-02-09 11:32:39 +0200

    Update content

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,11 +19,19 @@ variable "subscription_id" {
 variable "workload" {
   type        = string
   description = "Name of the workload that will be deployed"
+  validation {
+    condition     = length(var.workload) >= 2 && length(var.workload) <= 10
+    error_message = "Workload's length should be >= 2 and <= 10"
+  }
 }
 
 variable "environment" {
   type        = string
   description = "Name of the workload's environment"
+  validation {
+    condition     = length(var.workload) >= 2 && length(var.workload) <= 10
+    error_message = "Environment's length should be >= 2 and <= 10"
+  }
 }
 
 variable "location" {
